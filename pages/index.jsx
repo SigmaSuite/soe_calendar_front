71d390c0815d6dac75802dac9a45f9d3359696cd
nynon <nynonmaslo@gmail.com>
2021-10-21 19:01:11 +0200

    removed unnecessary files

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,30 +1,13 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 
-import { PERSONS } from '@endpoints/rustapi';
-import usePersonModals from '../src/components/ModalFunctions/PersonModals';
 import Layout from '../src/components/Layout/Layout';
 import GlobalStyles from '../src/styles/GlobalStyles';
 
 export default function PersonsPage() {
-  const [persons, setPersons] = useState();
-  const modals = usePersonModals(persons, setPersons);
-
-  async function fetchPersons() {
-    try {
-      const resp = await fetch(PERSONS);
-      const resJson = await resp.json();
-      setPersons(resJson);
-    } catch (error) {
-      console.error(error);
-    }
-  }
-
-  useEffect(() => fetchPersons(), []);
-
   return (
     <>
       <GlobalStyles />
-      <Layout modals={modals} />
+      <Layout />
     </>
   );
 }
---
+++ src/components/Calendar/DesktopIcons/DesktopIcons.jsx
@@ -0,0 +1,11 @@
+import PersonForm from '@components/PersonsManagment/PersonForm/PersonForm';
+import PersonsList from '@components/PersonsManagment/PersonsList/PersonsList';
+import CalendarMenu from '../CalendarMenu/CalendarMenu';
+
+export const LaunchCalendar = { component: <CalendarMenu />, name: 'Calendar' };
+export const LaunchAddPerson = { component: <PersonForm />, name: 'Add Person' };
+export const LaunchListPersons = { component: <PersonsList />, name: 'List Persons' };
+
+const DesktopIcons = [LaunchCalendar, LaunchAddPerson, LaunchListPersons];
+
+export default DesktopIcons;
