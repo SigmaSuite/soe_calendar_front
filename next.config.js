1abee544c08fb4144e68b422ab586bcbd0da3b52
szymekc <szymekc98@gmail.com>
2021-10-22 13:05:23 +0200

    hardcoded /calendar path

--- next.config.js
+++ next.config.js
@@ -1,12 +1,5 @@
 module.exports = {
   reactStrictMode: true,
   distDir: '_next',
-  async rewrites() {
-    return [
-      {
-        source: '/calendar/:path*',
-        destination: '/:path*',
-      },
-    ]
-  },
+  basePath: '/calendar',
 };
