3edc335a17d5c570563e68ab1044bced349127f1
szymekc <szymekc98@gmail.com>
2021-10-22 13:25:57 +0200

    removed rewrites

--- next.config.js
+++ next.config.js
@@ -2,12 +2,4 @@ module.exports = {
   reactStrictMode: true,
   distDir: '_next',
   basePath: '/calendar',
-  async rewrites() {
-    return [
-      {
-        source: '/:path*',
-        destination: '/calendar/:path*',
-      },
-    ]
-  },
 };
