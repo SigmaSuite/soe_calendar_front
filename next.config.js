3e0988b6323059c4262a042b20e78ee1de8f6c34
szymekc <szymekc98@gmail.com>
2021-10-22 13:13:26 +0200

    add rewrites

--- next.config.js
+++ next.config.js
@@ -2,4 +2,12 @@ module.exports = {
   reactStrictMode: true,
   distDir: '_next',
   basePath: '/calendar',
+  async rewrites() {
+    return [
+      {
+        source: '/:path*',
+        destination: '/calendar/:path*',
+      },
+    ]
+  },
 };
