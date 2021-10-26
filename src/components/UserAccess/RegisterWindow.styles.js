eb104a3417b7c6ef57c54d2075116c8d6953928d
nynon <nynonmaslo@gmail.com>
2021-10-26 15:49:37 +0200

    yet another change to background url

--- src/components/UserAccess/RegisterWindow.styles.js
+++ src/components/UserAccess/RegisterWindow.styles.js
@@ -17,7 +17,7 @@ export const RegisterWindowBody = styled(WindowBody)`
 
 export const RegisterImageContainer = styled.div`
   display: flex;
-  background-image: url('bartek.svg'),
+  background-image: url('calendar/bartek.svg'),
     ${({ isInvalid }) =>
       isInvalid
         ? 'linear-gradient(45deg,#e60a29,#6b6c6c)'
